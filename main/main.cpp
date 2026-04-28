c294a6296c6792b23e962f8f7a3d0eaadfb0dd6b
cpm <cpm.cyril@gmail.com>
2026-04-28 19:28:35 +0200

    bridge

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,13 @@
+#include "CommunicatorBridge.h"
+#include "STR.h"
+
+CTRBridge& bridge = CTRBridge::GetInstance();
+
 extern "C" void app_main(void)
 {
+	InitCores();
+	bridge.begin();
 
+	while (true)
+		bridge.Update();
 }
